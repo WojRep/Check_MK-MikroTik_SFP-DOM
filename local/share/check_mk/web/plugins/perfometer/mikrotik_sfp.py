05ca4785f50c4de27267f60021d42197db50c560
Wojciech Repiński <wrepinski@gmail.com>
2022-07-22 07:47:19 +0200

    fix perfometer (crash plugin)

--- local/share/check_mk/web/plugins/perfometer/mikrotik_sfp.py
+++ local/share/check_mk/web/plugins/perfometer/mikrotik_sfp.py
@@ -13,10 +13,8 @@ from math import fabs
 def perfometer_mikrotik_sfp(row, check_command, perf_data):
     for perf in perf_data:
         perf = list(perf)
-        if perf[0] == 'rx_power':
-            rx_power = float(perf[1])
-        if perf[0] == 'sfp_temp':
-            sfp_temp = float(perf[1])
+        rx_power = float(perf[1]) if perf[0] == 'rx_power' else -40.0 
+        sfp_temp = float(perf[1]) if perf[0] == 'sfp_temp' else 0.0
     return u"%s&nbsp;&nbsp;/&nbsp;&nbsp;%s&nbsp;&nbsp;&nbsp;&nbsp;" % (str('{0: .1f}'.format(rx_power)) + 'dBm', str('{0: .0f}'.format(sfp_temp)) + '°C'), perfometer_logarithmic_dual_independent(fabs(rx_power), '#54b948', 10, 2,
                                                                         fabs(sfp_temp), '#2098cb', 35, 2)
 
